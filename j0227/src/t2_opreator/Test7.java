31c90a4cde06d5f5a97eb5eaa1a021071e689830
sooYeon <hks3274@naver.com>
2024-02-27 17:54:58 +0900

    test

--- j0227/src/t2_opreator/Test7.java
+++ j0227/src/t2_opreator/Test7.java
@@ -7,9 +7,9 @@ public class Test7 {
 		//점수가 60점 이상은 합격, 60점 미만은 불합격
 		
 		Scanner sc = new Scanner(System.in); // 기본객체 안에 변수를 받아야 한다. 시스템이 알아서 받아달라고 하는거임, 입력받아주는 객체
+		//클래스변수, 객체변수
 		
 		char res;
-		
 		
 		System.out.print("점수를 입력하세요 : ");
 		int j = sc.nextInt();
