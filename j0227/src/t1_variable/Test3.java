14300779e4eef5bfb8ffd93a51aecf01f6547758
sooYeon <hks3274@naver.com>
2024-02-27 15:06:16 +0900

    test

--- j0227/src/t1_variable/Test3.java
+++ j0227/src/t1_variable/Test3.java
@@ -3,6 +3,8 @@ package t1_variable;
 public class Test3 {
 
 	public static void main(String[] args) {
+		
+		//정수형 타입(byte, char, short, int, long)
 		byte bb, bb2;
 		bb = 127;
 //		bb = 128; byte 타입의 수치 표현 범위: -127~128;
@@ -44,5 +46,14 @@ public class Test3 {
 		System.out.println("cc2 :"+ cc2);
 		System.out.println("cc3 :"+ cc3);
 	  System.out.println("cc4 :"+ cc4);
+	  
+	  char cc5 = '한';
+	  int cc6 = '한';
+	  System.out.println("cc5 :"+cc5);
+	  System.out.println("cc6 :"+cc6);
+	  
+	  char cc7 = '\u0041';
+	  System.out.println("cc7 :"+cc7);
+	  
 	}
 }
